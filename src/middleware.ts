ee0aaf553ca2c73c1db9ef7f3ccc40ccab294204
Tejasva Jaitly <tejasvajaitly@gmail.com>
2025-03-03 17:57:49 -0800

    fix: handle dynamic preview urls

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,13 +6,24 @@ export const config = {
 
 export default async function middleware(req: NextRequest) {
   const rootDomain = process.env.NEXT_PUBLIC_ROOT_DOMAIN;
-  const previewDomain = process.env.NEXT_PUBLIC_PREVIEW_DOMAIN;
+  const previewDomain = process.env.NEXT_PUBLIC_PREVIEW_DOMAIN!;
   const url = req.nextUrl;
   const host = req.headers.get("host");
-  let subdomain = host?.replace(`.${previewDomain}`, "");
+  let subdomain;
+  if (host?.endsWith(previewDomain)) {
+    const hostSegments = host.split(".");
+    if (hostSegments.length === 4) {
+      subdomain = hostSegments[0];
+      return NextResponse.rewrite(
+        new URL(`/${subdomain}${url.pathname}`, req.url)
+      );
+    } else {
+      return NextResponse.next();
+    }
+  }
   subdomain = host?.replace(`.${rootDomain}`, "");
 
-  if (subdomain !== rootDomain && subdomain !== previewDomain) {
+  if (subdomain !== rootDomain) {
     return NextResponse.rewrite(
       new URL(`/${subdomain}${url.pathname}`, req.url)
     );
