47d9fb51b12981bd64eb428b79daea4135732dba
Tejasva Jaitly <tejasvajaitly@gmail.com>
2025-03-07 14:31:02 -0800

    onboarding cards init

---
+++ src/app/onboarding/org-card.tsx
@@ -0,0 +1,38 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function OrgCard({
+  activeStep,
+  setActiveStep,
+}: {
+  activeStep: number;
+  setActiveStep: (step: number) => void;
+}) {
+  return (
+    <Card
+      onClick={() => setActiveStep(1)}
+      className={`w-full text-sm cursor-pointer ${
+        activeStep === 1
+          ? `border border-neutral-950 dark:border-neutral-50`
+          : ``
+      }`}
+    >
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,3 +1,59 @@
+"use client";
+
+import OrgCard from "./org-card";
+import GithubCard from "./github-card";
+import ChangelogCard from "./changelog-card";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
 export default function Page() {
-  return <p>this will be the onboarding page</p>;
+  const [activeStep, setActiveStep] = useState<number>(1);
+  return (
+    <div className="flex flex-col justify-start items-center w-full sm:w-[450px] gap-4">
+      <Header />
+      <OrgCard activeStep={activeStep} setActiveStep={setActiveStep} />
+      <GithubCard activeStep={activeStep} setActiveStep={setActiveStep} />
+      <ChangelogCard activeStep={activeStep} setActiveStep={setActiveStep} />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <div className="w-full flex flex-row justify-start items-center">
+        <Image
+          src="/logo.svg"
+          className="dark:invert"
+          alt="Nebula logo"
+          width={30}
+          height={30}
+          priority
+        />
+      </div>
+
+      <div className="w-full flex flex-col justify-start items-start">
+        <p className="text-2xl font-extrabold">Hello</p>
+        <p className="text-muted-foreground">
+          Let's set up your first changelog
+        </p>
+      </div>
+    </>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="w-full flex flex-row justify-start items-center gap-2 mt-8">
+      <p className="text-muted-foreground text-sm">Need help?</p>
+      <Link
+        className="text-primary text-sm"
+        href="mailto:tejasvajaitly@gmail.com"
+      >
+        Contact support
+      </Link>
+    </div>
+  );
 }
